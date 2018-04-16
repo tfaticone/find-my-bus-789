8c8c5ac8a6d409d0cffe0f913273731ebb9fd844
Alex Bowen <caffinefree19@yahoo.com>
2018-04-16 07:07:04 -0400

    tieing in the new functionality into info page

--- app/src/main/java/edu/rit/se/www/findmybus/CurrentBusInfoActivity.java
+++ app/src/main/java/edu/rit/se/www/findmybus/CurrentBusInfoActivity.java
@@ -17,11 +17,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.speech.tts.TextToSpeech;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Locale;
 import java.net.URI;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import edu.rit.se.www.findmybus.API.RouteConnection;
 
 public class CurrentBusInfoActivity extends AppCompatActivity {
     TextToSpeech talker;
@@ -29,6 +43,8 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
     private int distance = 0;
     private int heading = 0;
     Timer timer;
+    TextView routeListText;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +61,8 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_current_bus_info);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         final Bundle bundle = getIntent().getExtras();
 
@@ -54,7 +70,11 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
             TextView routeIDText = (TextView) findViewById(R.id.routeID);
             routeID = Integer.parseInt(bundle.getString("routeID"));
             routeIDText.setText(bundle.getString("routeID"));
+            TextView routeListText = (TextView) findViewById(R.id.routeList);
         }
+
+        requestQueue = Volley.newRequestQueue(this);  // This setups up a new request queue which we will need to make HTTP requests.
+        getRouteList(routeID);
 
     }
 
@@ -129,5 +149,57 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getRouteList(Integer routeNumber) {
+        // First, we insert the username into the repo url.
+        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
+        String url = "http://api.rgrta.com/rtroutes?key=d0eac034-06b4-4c51-877a-3f21119b87e7&routeid=" + routeNumber;
+
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String routeName = jsonObj.get("RouteName").toString();
+//                                    routeListText.setText(routeNumber);
+//                                    String currentText = routeListText.getText().toString();
+//                                    routeListText.setText(currentText + "\n\n" + routeNumber);
+                                    Log.e("API", routeName);
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            routeListText.setText("No buses found on this route.");
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        routeListText.setText("Unfortunately, we have encountered an error. ");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
     }
 }
