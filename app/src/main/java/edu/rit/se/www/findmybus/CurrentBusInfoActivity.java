3b4ca5c6759a6bb1028da3f6f439bdb67f104aee
Thomas Faticone <tjf4881@rit.edu>
2018-04-17 00:57:27 -0400

    Added ability for vibration

--- app/src/main/java/edu/rit/se/www/findmybus/CurrentBusInfoActivity.java
+++ app/src/main/java/edu/rit/se/www/findmybus/CurrentBusInfoActivity.java
@@ -4,8 +4,10 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
@@ -22,13 +24,22 @@ import java.net.URI;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import android.os.Vibrator;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class CurrentBusInfoActivity extends AppCompatActivity {
     TextToSpeech talker;
     Integer routeID = null;
-    private int distance = 0;
-    private int heading = 0;
+    private float distance = 0;
+    private float heading = 0;
     Timer timer;
+    Timer vibTimer = null;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static Location savedLocation = null;
+    private Vibrator myVib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +54,15 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
             }
         });
 
+        myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
         setContentView(R.layout.activity_current_bus_info);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         final Bundle bundle = getIntent().getExtras();
 
@@ -56,11 +72,33 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
             routeIDText.setText(bundle.getString("routeID"));
         }
 
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                savedLocation = location;
+                            }
+                        }
+                    });
+        } catch (SecurityException se) {
+
+        }
+
+        Button setLocationBtn = (Button) findViewById(R.id.currentLocation);
+        setLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveCurrentLocation();
+            }
+        });
+
     }
 
     protected void onStart() {
         super.onStart();
-        getValues();
         updateDisplayValues();
     }
 
@@ -74,32 +112,92 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
         startTimedUpdates();
     }
 
-    private void getValues() {
-        Random r = new Random();
-        distance = r.nextInt((10) + 1);
-
-        heading = r.nextInt(360 + 1);
-    }
-
     private void updateDisplayValues(){
         final TextView busDistanceLabel = (TextView) findViewById(R.id.busDistance);
         final TextView busHeadingLabel = (TextView) findViewById(R.id.busHeading);
 
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                distance = (float)(((double)location.distanceTo(savedLocation)) * 0.000621371);
+                                heading = location.bearingTo(savedLocation);
+                            }
+                        }
+                    });
+        } catch (SecurityException se) {
+
+        }
+
         CurrentBusInfoActivity.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                busDistanceLabel.setText(Integer.toString(distance) + " miles");
-                busHeadingLabel.setText(Integer.toString(heading) + " degrees");
+                busDistanceLabel.setText(Float.toString(distance) + " miles");
+                busHeadingLabel.setText(Float.toString(heading) + " degrees");
+                if(distance < 0.0189394) { //Rougly 100 feet in miles
+                    startVibrationTimer(distance, heading);
+                } else {
+                    stopVibrationTimer();
+                }
                 if(getVoicePreference()){
-                    //voiceUpdates();
+                    voiceUpdates();
                 }
             }
         });
     }
 
+    private void saveCurrentLocation() {
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                savedLocation = location;
+                            }
+                        }
+                    });
+        } catch (SecurityException se) {
+        }
+    }
+
     private void voiceUpdates() {
-        talker.speak("Bus" + Integer.toString(routeID) + " is " + Integer.toString(distance)
-                + " miles away. It has a heading of " + Integer.toString(heading) + " degrees.", TextToSpeech.QUEUE_FLUSH, null);
+        if(!talker.isSpeaking()) {
+            talker.speak("Bus" + Integer.toString(routeID) + " is " + Float.toString(distance)
+                    + " miles away. It has a heading of " + Float.toString(heading) + " degrees.", TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    private void stopVibrationTimer() {
+        if(vibTimer != null) {
+            vibTimer.cancel();
+            vibTimer = null;
+        }
+    }
+
+    private void startVibrationTimer(Float distance, Float bearing) {
+        stopVibrationTimer();
+
+        vibTimer = new Timer();
+
+        Float time = (float)((10 * (distance * 0.000189394)) + 2);
+        Float strength = (float)((-.009 * (distance * 0.000189394)) + 1);
+        final VibrationEffect effect = VibrationEffect.createOneShot(Math.round(time), Math.round(strength * VibrationEffect.DEFAULT_AMPLITUDE));
+
+        TimerTask vibrateTask = new TimerTask() {
+            @Override
+            public void run() {
+                if(myVib.hasVibrator()) {
+                    myVib.vibrate(effect);
+                }
+            }
+        };
+
+        vibTimer.schedule(vibrateTask, 0, 2 * Math.round(time));
     }
 
     private void startTimedUpdates(){
@@ -111,7 +209,7 @@ public class CurrentBusInfoActivity extends AppCompatActivity {
             }
         };
 
-        timer.schedule(updateData, 20000, 20000);
+        timer.schedule(updateData, 1000, 10000);
     }
 
     private boolean getVoicePreference() {
